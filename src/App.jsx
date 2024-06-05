19693e5c38da2b4a746543eed5b38c90b8018121
marcosvitordev <marcosvitorlima531@gmail.com>
2024-06-05 01:12:49 -0500

    Primeiro dia

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MenuInicio from "./components/MenuInicio"
+
+
+function App() {
+
+  return (
+    <div>
+      <MenuInicio />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/MenuInicio.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const MenuInicio = () => {
+  return (
+    <div>
+      <h1>Teste</h1>
+    </div>
+  )
+}
+
+export default MenuInicio
