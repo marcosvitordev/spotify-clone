e13dd7906eaadcbab5a989f1ff7c3627cb057aa6
marcosvitordev <marcosvitorlima531@gmail.com>
2024-06-05 02:08:20 -0500

    Duas da madrugada

--- src/components/MenuInicio.jsx
+++ src/components/MenuInicio.jsx
@@ -1,12 +1,20 @@
-
+import { AiFillHome, AiOutlineSearch } from "react-icons/ai"
 import React from 'react'
 
 const MenuInicio = () => {
-  return (
-    <div>
-      <h1>Teste</h1>
-    </div>
-  )
+    return (
+        <div className="m-2 flex flex-col w-[300px] bg-gray-500 text-white h-[100px]">
+
+            <div className="flex items-center gap-2 text-2xl">
+                <AiFillHome fontSize={20} />
+                <p>Inicio</p>
+            </div>
+            <div className="flex items-center gap-2 text-2xl">
+                <AiOutlineSearch fontSize={20} />
+                <p>Buscar</p>
+            </div>
+        </div>
+    )
 }
 
 export default MenuInicio
